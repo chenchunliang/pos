acecd0094950f5a5f80cb0d362d68ad26896ebf6
chenchunliang <peter60146@gmail.com>
2018-11-23 14:40:00 +0800

    修改view

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,33 +1,16 @@
-<!--Footer section-->
-<section class="footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-12">
-        <div class="main_footer">
-          <div class="row">
-            <div class="col-sm-4 col-xs-12">
-              <div class="flowus" style="padding-left: 10px;"> <a href="https://www.facebook.com/helloagric/"><i class="fa fa-facebook" title="粉絲專業"></i></a> <a href="https://www.youtube.com/channel/UClgKxUg4V5zObtfX83aJmWA"><i class="fa fa-youtube" title="YouTube"></i></a> <a href="https://www.agric.tw/"><i class="fa fa-globe" title="電商官網"></i></a> </div>
-            </div>
-            <div class="col-sm-4 col-xs-12">
-              <div class="copyright_text" style="text-align:center">
-                <p><a style="color:#fff;" href="{{url('index')}}">阿古力社會企業</a></p>
-              </div>
-            </div>
-            <div class="col-sm-4 col-xs-12">
-              <div class="copyright_text">
-                <p class=" wow fadeInRight" data-wow-duration="1s" style="font-size:12px;"><a style="color:#bbbbbb;" href="http://bootstrapthemes.co">Bootstrap Themes</a>2016. All Rights Reserved</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<!-- End off footer Section-->
-</div>
-<!-- START SCROLL TO TOP  -->
 
-<div class="scrollup" title="回到最頂部"> <a href="#"><i class="fa fa-chevron-up"></i></a> </div>
+    <!-- jQuery -->
+	{{ HTML::script('vendor/jquery/jquery.min.js')}}
+
+    <!-- Bootstrap Core JavaScript -->
+    {{ HTML::script('vendor/bootstrap/js/bootstrap.min.js')}}
+
+    <!-- Metis Menu Plugin JavaScript -->
+    {{ HTML::script('vendor/metisMenu/metisMenu.min.js')}}
+
+    <!-- Custom Theme JavaScript -->
+    {{ HTML::script('dist/js/sb-admin-2.js')}}
+
 </body>
+
 </html>
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,80 +1,33 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="">
-<title>{{$title}}</title>
-	{{ HTML::style('css/iconfont.css')}}
-    {{ HTML::style('css/slick/slick.css')}}
-    {{ HTML::style('css/slick/slick-theme.css')}}
-    {{ HTML::style('css/stylesheet.css')}}
-    {{ HTML::style('css/font-awesome.min.css')}}
-    {{ HTML::style('css/jquery.fancybox.css')}}
-    {{ HTML::style('css/bootstrap.css')}}
-    {{-- HTML::style('css/bootstrap.min.css')--}}
-    {{ HTML::style('css/magnific-popup.css')}}
 
-    <!--For Plugins external css-->
-    {{ HTML::style('css/plugins.css')}}
-    
-    <!--Theme custom css -->
-    {{ HTML::style('css/style.css')}}
-    
-    <!--Theme Responsive css-->
-    {{ HTML::style('css/responsive.css')}}
-    
-    {{ HTML::style('css/else/my.css')}}
-    
-    {{ HTML::script('js/vendor/modernizr-2.8.3-respond-1.4.2.min.js')}}
-    
-    {{ HTML::script('js/vendor/jquery-1.11.2.min.js')}}
-	{{ HTML::script('js/vendor/bootstrap.min.js')}}
-        
-	{{ HTML::script('js/jquery.magnific-popup.js')}}
-    {{ HTML::script('js/jquery.mixitup.min.js')}}
-	{{ HTML::script('js/jquery.easing.1.3.js')}}
-    {{ HTML::script('js/jquery.masonry.min.js')}}
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <!--slick slide js -->
-    {{ HTML::script('js/slick.js')}}
-    {{ HTML::script('js/slick.min.js')}}
-            
-    {{ HTML::script('js/plugins.js')}}
-    {{ HTML::script('js/main.js')}}
+    <title>現金銷售管理平台</title>
+
+    <!-- Bootstrap Core CSS -->
+    {{ HTML::style('vendor/bootstrap/css/bootstrap.min.css')}}
+
+    <!-- MetisMenu CSS -->
+    {{ HTML::style('vendor/metisMenu/metisMenu.min.css')}}
+
+    <!-- Custom CSS -->
+    {{ HTML::style('dist/css/sb-admin-2.css')}}
+
+    <!-- Custom Fonts -->
+    {{ HTML::style('vendor/font-awesome/css/font-awesome.min.css')}}
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
-<body data-spy="scroll" data-target=".navbar-collapse">
-<div class='preloader'>
-  <div class='loaded'>&nbsp;</div>
-</div>
-<div class="culmn">
-<header id="main_menu" class="header navbar-fixed-top menu-scroll">
-  <div class="main_menu_bg">
-    <div class="container">
-      <div class="row">
-        <div class="nave_menu">
-          <nav class="navbar navbar-default">
-            <div class="container-fluid"> 
-              <!-- Brand and toggle get grouped for better mobile display -->
-              <div class="navbar-header">
-                <button type="button" id="mobile_btn" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-                <a class="navbar-brand" href="{{url('/')}}"> {{ HTML::image('images/WaterMark2.png', 'Logo') }} </a> </div>
-              
-              <!-- Collect the nav links, forms, and other content for toggling -->
-              
-              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> @if(!empty($items))
-                <ul class="nav navbar-nav navbar-right">
-                  <li><a href="#home">產品介紹</a></li>
-                  <li><a href="#inspection">檢驗報告</a></li>
-                  <li><a href="#produce">生產紀錄</a></li>
-                  <li><a href="#environment">農友與生態環境介紹</a></li>
-                </ul>
-                @endif </div>
-            </div>
-          </nav>
-        </div>
-      </div>
-    </div>
-  </div>
-</header>
-<!--End of header -->
