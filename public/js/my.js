6a14f91572620ba2d5a5a4d262cf9caba608bc20
chenchunliang <peter60146@gmail.com>
2018-11-23 17:21:38 +0800

    新增

---
+++ public/js/my.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+$(document).ready(function(){
+	$('.deletebtn').click(function() {
+		//console.log('你確定刪除此項目?');
+		var ans =confirm("你確定刪除此項目?");
+		
+		if(ans){
+			var formid=$(this).data('formid');
+			console.log(formid);
+			$('#'+formid).submit();
+		}
+	});
+
+	$('#changePassword').click(function() {
+		if($('#changePassword_tr').css('display')=="none"){
+			$('#admin_password').prop('disabled',false);
+		}else{
+			$('#admin_password').prop('disabled',true);
+		}
+		$('#changePassword_tr').toggle(1000);		
+	});
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
