e6b4311d2e02378c5cec90f82b2282dc99c6938c
chenchunliang <peter60146@gmail.com>
2018-11-30 13:33:04 +0800

    增加品項明細table

---
+++ resources/views/salesinvoice/display_detail.blade.php
@@ -0,0 +1,28 @@
+<div style="padding:3%">
+  <h3>品項明細</h3>
+  <table width="100%" class="table table-striped table-bordered table-hover">
+    <thead>
+      <tr>
+        <th>品項</th>
+        <th >數量</th>
+        <th>單價</th>
+      </tr>
+    </thead>
+    <tbody>
+    @foreach(json_decode($Salesinvoice->salesinvoice_productarray) as $productArray)
+    <tr>
+      <td align="center">{{$productArray->ProductName}}</td>
+      <td align="center">{{$productArray->ProductQty}}</td>
+      <td align="center">{{$productArray->ProductAmount}}</td>
+    </tr>
+    @endforeach
+    </tbody>
+    
+  </table>
+  <div align="right">
+      <p>免稅銷售額：{{ number_format($Salesinvoice->salesinvoice_tnsalesamount) }}</p>
+      <p>應稅銷售額：{{ number_format($Salesinvoice->salesinvoice_txsalesamount) }}</p>
+      <p>稅額：{{ number_format($Salesinvoice->salesinvoice_taxamount) }}</p>
+      <p>總金額：{{ number_format($Salesinvoice->salesinvoice_totalamount) }}</p>
+  </div>
+</div>
