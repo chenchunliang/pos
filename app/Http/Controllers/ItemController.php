8fab0ca43b54652b32116f10f7631ff30a45c02f
chenchunliang <peter60146@gmail.com>
2018-12-03 15:16:53 +0800

    完成品項controller

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Item;
+use App\Parameter;
 
 class ItemController extends Controller
 {
@@ -16,7 +17,9 @@ class ItemController extends Controller
     {
         //
 		$Items=Item::all();
-		return view('item.index',compact('Items'));
+		$Parameters=Parameter::where('parameter_groups','價格')->get();
+		$text=array('','一','二','三','四','五');
+		return view('item.index',compact('Items','Parameters','text'));
 		
     }
 
@@ -28,7 +31,9 @@ class ItemController extends Controller
     public function create()
     {
         //
-		return view('item.create');
+		$text=array('','一','二','三','四','五');
+		$Parameters=Parameter::where('parameter_groups','價格')->get();
+		return view('item.create',compact('Parameters','text'));
     }
 
     /**
@@ -44,6 +49,11 @@ class ItemController extends Controller
 			'item_name'=>'required',
 			'item_specification'=>'required',
 			'item_barcode'=>'required',
+			'item_unitprice1'=>'required',
+			'item_unitprice2'=>'required',
+			'item_unitprice3'=>'required',
+			'item_unitprice4'=>'required',
+			'item_unitprice5'=>'required',
 			'item_unit'=>'required',
 			'item_taxtype'=>'required|min:1|max:2',
 			'item_image'=>'required',
@@ -75,8 +85,10 @@ class ItemController extends Controller
     public function edit($id)
     {
         //
+		$text=array('','一','二','三','四','五');
+		$Parameters=Parameter::where('parameter_groups','價格')->get();
 		$Item=Item::find($id);
-		return view('item.edit',compact('Item'));
+		return view('item.edit',compact('Item','Parameters','text'));
     }
 
     /**
@@ -94,10 +106,18 @@ class ItemController extends Controller
 			'item_name'=>'required',
 			'item_specification'=>'required',
 			'item_barcode'=>'required',
+			'item_unitprice1'=>'required',
+			'item_unitprice2'=>'required',
+			'item_unitprice3'=>'required',
+			'item_unitprice4'=>'required',
+			'item_unitprice5'=>'required',
 			'item_unit'=>'required',
 			'item_taxtype'=>'required|min:1|max:2',
-			'item_image'=>'required',
 		]);
+		
+		if($request->item_image){
+			$Item->item_image=$request->item_image;
+		}
 		
 		$Item->update($validated);
 		
