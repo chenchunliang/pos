271c862b1e8ce7247ece8f4e9b116c6bb911c47b
chenchunliang <peter60146@gmail.com>
2018-12-03 15:17:20 +0800

    增加售價1~5欄位

--- database/migrations/2018_11_22_083703_create_items_table.php
+++ database/migrations/2018_11_22_083703_create_items_table.php
@@ -18,9 +18,14 @@ class CreateitemsTable extends Migration
 			$table->string('item_name');
 			$table->string('item_specification');
 			$table->string('item_barcode');
+			$table->integer('item_unitprice1');
+			$table->integer('item_unitprice2');
+			$table->integer('item_unitprice3');
+			$table->integer('item_unitprice4');
+			$table->integer('item_unitprice5');
 			$table->string('item_unit');
 			$table->integer('item_taxtype');
-			$table->longText('item_image');
+			$table->longText('item_image')->nullable($value = true);
             $table->timestamps();
 			$table->softDeletes();
         });
