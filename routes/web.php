8c9bfdc37aecd497e4cea013b9121d305cf765ff
chenchunliang <peter60146@gmail.com>
2018-12-06 08:53:42 +0800

    增加發票上傳、發票作廢 url

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::resource('invoice', 'InvoiceController');
 //ajax 一定要比resource('salesinvoice')還要前面
 Route::get('/salesinvoice/display_detail/{id}', 'SalesinvoiceController@display_detail');
 Route::get('/salesinvoice/sales', 'SalesinvoiceController@sales');
+Route::get('/salesinvoice/show/{id}/type/{type}', 'SalesinvoiceController@show');
+Route::get('/salesinvoice/uploadC0401', 'SalesinvoiceController@uploadC0401');
 
 Route::resource('salesinvoice', 'SalesinvoiceController');
+
+Route::get('/invalidinvoice/uploadC0501', 'InvalidinvoiceController@uploadC0501');
 Route::resource('invalidinvoice', 'InvalidinvoiceController');
